cd45e5a8c1b308f511f213773fb4d0652f3a16d4
Kshitij Pawar <kshitijp@DESKTOP-7SLMOGK>
2024-02-24 00:24:29 +0530

    set protocol to http for checkout success callback in every env

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -8,7 +8,7 @@ export function cn(...inputs: ClassValue[]) {
 let hostUrl: string;
 export const getHostUrl = () => {
   if (!hostUrl) {
-    hostUrl = `${process.env.NODE_ENV === "development" ? "http://" : "https://"}${process.env.NEXT_PUBLIC_VERCEL_URL ?? "localhost:3000"}`;
+    hostUrl = `${"http://"}${process.env.NEXT_PUBLIC_VERCEL_URL ?? "localhost:3000"}`;
   }
   return hostUrl;
 };
