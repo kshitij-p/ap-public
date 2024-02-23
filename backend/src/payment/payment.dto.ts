0f097164a6f758855c5881fa4af01465eda4fb0c
Kshitij Pawar <kshitijp@DESKTOP-7SLMOGK>
2024-02-24 01:21:07 +0530

    update validation

--- backend/src/payment/payment.dto.ts
+++ backend/src/payment/payment.dto.ts
@@ -5,12 +5,14 @@ import { z } from 'nestjs-zod/z';
 export const paymentProviderSchema = z.enum(['STRIPE']);
 
 export const CreatePaymentSchema = z.object({
-  items: z.array(
-    z.object({
-      productId: z.string().min(1),
-      quantity: z.number().min(1),
-    }),
-  ),
+  items: z
+    .array(
+      z.object({
+        productId: z.string().min(1),
+        quantity: z.number().min(1),
+      }),
+    )
+    .min(1),
   successUrl: z.string().min(1),
   provider: paymentProviderSchema,
 });
